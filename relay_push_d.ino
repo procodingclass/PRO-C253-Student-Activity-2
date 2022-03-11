c7abdbe911bc013f23d52d248a0369ace8223bd4
procodingclass <88050205+procodingclass@users.noreply.github.com>
2022-03-11 16:38:41 +0530

    Update relay_push_d.ino

--- relay_push_d.ino
+++ relay_push_d.ino
@@ -1,25 +1,18 @@
-/*
- * This ESP32 code is created by esp32io.com
- *
- * This ESP32 code is released in the public domain
- *
- * For more detail (instruction and wiring diagram), visit https://esp32io.com/tutorials/esp32-button-toggle-relay
- */
+
 
 #include <ezButton.h>
 
-#define BUTTON_PIN 22  // ESP32 pin GIOP22 connected to button's pin
-#define RELAY_PIN  18  // ESP32 pin GIOP27 connected to relay's pin
+#define BUTTON_PIN 22  
+#define RELAY_PIN  18  
 
 ezButton button(BUTTON_PIN);  // create ezButton object that attach to pin 7;
 
-// The below are variables, which can be changed
 int relay_state = LOW;   // the current state of relay
 
 void setup() {
-  Serial.begin(9600);         // initialize serial
-  pinMode(RELAY_PIN, OUTPUT); // set ESP32 pin to output mode
-  button.setDebounceTime(50); // set debounce time to 50 milliseconds
+  Serial.begin(9600);         
+  pinMode(RELAY_PIN, OUTPUT); 
+  button.setDebounceTime(50); 
 }
 
 void loop() {
@@ -27,11 +20,9 @@ void loop() {
 
   if (button.isPressed()) {
     Serial.println("The button is pressed");
-
-    // toggle state of relay
+   
     relay_state = !relay_state;
 
-    // control relay arccoding to the toggled state
     digitalWrite(RELAY_PIN, relay_state);
   }
 }
